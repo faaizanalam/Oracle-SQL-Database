1c1ca483e6dc696b61c6de8a26531f43be0a7534
Faizan <faizanalam695@gmail.com>
2025-11-08 02:01:28 +0500

    16th Commit
    Files updated.

--- Extended_Excercise_P3.sql
+++ Extended_Excercise_P3.sql
@@ -32,9 +32,56 @@ SELECT TO_NUMBER('45,000', '9,999,999,999') FROM dual;
 SELECT TO_NUMBER('$45,000', '$9,999,999,999') FROM dual;
 SELECT TO_NUMBER('$45,000.002', '$9,999,999,999.999999') FROM dual;
 SELECT TO_NUMBER('String') FROM DUAL;   -----error
+SELECT TO_NUMBER('$55,000', 'L99,999') FROM DUAL; ----------L is used for currency;
+
+SELECT * FROM nls_session_parameters;
+
+//SELECT TO_NUMBER(45000, '00000000') FROM dual;
 
 ----------------------------------------------TO_NUMBER--------------------------------------------------
 SELECT TO_DATE('May 22, 1999', 'MM DD, YYYY') FROM DUAL;
 SELECT TO_DATE('22/12/23', 'DD-MM-YY') FROM DUAL;
+
+
+----------------------------------------------NULL HANDLING--------------------------------------------------
+---------------------------------------------------NVL-------------------------------------------------------
+SELECT E_id, NVL(TO_CHAR(leaving_date, 'DD-MM-YYYY'), 'Working Employee') FROM EMPLOYEEES;
+SELECT * FROM EMPLOYEEES;
+
+SELECT * FROM EMPLOYEES;
+SELECT EMPLOYEE_ID, First_name, Last_name, NVL(commission_pct, 'NOT Assigned yet') FROM employees;  -----String cannot be converted to Number
+SELECT EMPLOYEE_ID, First_name, Last_name, NVL(commission_pct, '0') FROM employees;   ------
+SELECT * FROM EMPLOYEES;
+
+-------------------------------------------- ------NVL2-------------------------------------------------------
+SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, NVL2(commission_pct, Salary*commission_pct, '0')FROM EMPLOYEES;
+SELECT * FROM EMPLOYEES;
+
+
+---------------------------------------------------COALESCE---------------------------------------------------
+SELECT COALESCE(First_name, Last_name, Email, Phone_number) AS COALESCE FROM EMPLOYEES;
+SELECT COALESCE(Commission_pct, Employee_id, SALARY) AS COALESCE FROM EMPLOYEES;
+
+----------------------------------------------------NULLIF----------------------------------------------------
+SELECT NULLIF(123,123) FROM dual;
+SELECT NULLIF('Faizan', 'Faizan') FROM dual;    ------Return 1st parameter when there is not a match.
+SELECT NULLIF('hello', 'Hello') FROM dual;
+
+-----------------------------------------------------LNNVL----------------------------------------------------
+SELECT * FROM EMPLOYEEES 
+WHERE LNNVL(leaving_date = NULL);
+
+SELECT * FROM EMPLOYEEES 
+WHERE LNNVL(leaving_date <> NULL);
+
+SELECT * FROM EMPLOYEEES 
+WHERE LNNVL(leaving_date IS NULL);
+
+SELECT * FROM EMPLOYEEES 
+WHERE LNNVL(leaving_date IS NOT NULL);
+
+-----------------------------------------------------Conditional Functions-----------------------------------------
+---------------------------------------------------------Case Statement--------------------------------------------
+
 
 
