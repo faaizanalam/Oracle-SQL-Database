8f31f30ed0cccb7248f49d664c68d245b1f79ed5
Faizan <faizanalam695@gmail.com>
2026-03-01 05:10:19 +0500

    88th Commit.
    Files Updated.

---
+++ PL_SQL_Recap.sql
@@ -0,0 +1,12 @@
+------------------------------------*******************************-----------------------------------
+--Write a PL/SQL block that:
+--Accepts employee details
+--Validates:
+--Age between 18–60
+--Salary >= department minimum
+--Joining date not in future
+--If any fails → GOTO cleanup section
+--Cleanup must log failure
+--Otherwise → print "Validation Successful"
+
+
